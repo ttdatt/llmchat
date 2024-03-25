f68fae8913dcac5e098474235879a17041ea9b3d
dat.tran <ttdat.nt@gmail.com>
2024-03-25 20:15:28 +0700

    sort messages

--- src/components/MessageArea.tsx
+++ src/components/MessageArea.tsx
@@ -2,6 +2,7 @@ import { ChatMessage } from './chat/ChatMessage';
 import { useAppStore } from '../store';
 import { ChatInput } from './ChatInput';
 import './MessageArea.css';
+import sortBy from 'lodash/sortBy';
 
 export const MessageArea = () => {
 	const currentThread = useAppStore(
@@ -13,7 +14,7 @@ export const MessageArea = () => {
 	return (
 		<div className='container'>
 			<div style={{ flex: 1 }}>
-				{Object.values(currentThread.messages).map((x) => {
+				{sortBy(Object.values(currentThread.messages), 'timestamp').map((x) => {
 					return <ChatMessage key={x.id} text={x.text} owner={x.owner} />;
 				})}
 			</div>
