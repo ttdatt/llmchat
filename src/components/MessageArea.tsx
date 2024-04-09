d9614a5c5e0a82b0c6e257a1f9cfeb1cdc16bcea
Rudy Huynh <rudy.huynh286@gmail.com>
2024-04-09 22:28:39 +0700

    resolve conflict

--- src/components/MessageArea.tsx
+++ src/components/MessageArea.tsx
@@ -3,14 +3,19 @@ import { ChatInput } from './ChatInput';
 import orderBy from 'lodash/orderBy';
 import { useAtomValue } from 'jotai';
 import { currentThreadAtom } from '@/atom/atoms';
+import cls from 'classnames';
 
-export const MessageArea = () => {
+export const MessageArea = ({ isMobile }: { isMobile: boolean }) => {
   const currentThread = useAtomValue(currentThreadAtom);
 
   if (!currentThread) return null;
 
   return (
-    <div className='flex flex-col w-full overflow-auto'>
+    <div
+      className={cls(
+        'flex flex-col overflow-auto',
+        isMobile ? 'h-full' : 'w-full',
+      )}>
       <div className='flex-1 overflow-y-auto overscroll-none'>
         {orderBy(Object.values(currentThread.messages), 'timestamp', 'asc').map(
           (x) => {
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -10,8 +10,12 @@ import {
   threadsAtom,
 } from '@/atom/atoms';
 import orderBy from 'lodash/orderBy';
+import { HamburgerMenuIcon } from '../../assets/HamburgerMenuIcon';
+import { XIcon } from '../../assets/XIcon';
+import cls from 'classnames';
+import { useState } from 'react';
 
-export function Navbar() {
+export function Navbar({ isMobile }: { isMobile: boolean }) {
   const threads = useAtomValue(threadsAtom);
   const currentThreadId = useAtomValue(currentThreadIdAtom);
   const createThread = useSetAtom(createNewThreadAtom);
@@ -21,17 +25,45 @@ export function Navbar() {
     (t) => <NavItem key={t.id} thread={t} currentThreadId={currentThreadId} />,
   );
 
+  const [shouldShowNavbar, setShouldShowNavbar] = useState(!isMobile);
+
+  const onClickHamburger = () => {
+    setShouldShowNavbar(true);
+  };
+
+  const onClose = () => setShouldShowNavbar(false);
+
   return (
-    <div className={classes.navbar}>
-      <div className='flex flex-col'>
-        <Button onClick={() => createThread()}>New chat</Button>
-      </div>
-      <div className='flex-1 mt-4'>{links}</div>
-      <div>
-        <UnstyledButton onClick={() => openSettings(true)}>
-          <IconSettings className='fill-white' />
-        </UnstyledButton>
-      </div>
-    </div>
+    <>
+      {isMobile ? (
+        <div className={classes.mobileToolbar}>
+          <span onClick={onClickHamburger}>
+            <HamburgerMenuIcon />
+          </span>
+        </div>
+      ) : null}
+      {shouldShowNavbar ? (
+        <div
+          className={cls(classes.navbar, { [classes.navbarMobile]: isMobile })}>
+          {isMobile ? (
+            <>
+              <div style={{ height: 40 }} />
+              <div className={classes.close} onClick={onClose}>
+                <XIcon />
+              </div>
+            </>
+          ) : null}
+          <div className='flex flex-col'>
+            <Button onClick={() => createThread()}>New chat</Button>
+          </div>
+          <div className='flex-1 mt-4'>{links}</div>
+          <div>
+            <UnstyledButton onClick={() => openSettings(true)}>
+              <IconSettings className='fill-white' />
+            </UnstyledButton>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 }
