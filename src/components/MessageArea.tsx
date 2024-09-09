ff44fe81f7288409cc1365368b1245bbd5d8209d
dat.tran <ttdat.nt@gmail.com>
2024-09-09 08:19:43 +0700

    update

--- src/components/MessageArea.tsx
+++ src/components/MessageArea.tsx
@@ -5,20 +5,25 @@ import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 import { createNewThreadAtom, currentThreadAtom } from '@/atom/derivedAtoms';
 import { useMobile } from '@/hooks/useMobile';
 import { Burger } from '@mantine/core';
-import { drawerAtom } from '@/atom/atoms';
+import { drawerAtom, llmTokensAtom } from '@/atom/atoms';
 import { IconEdit } from '@tabler/icons-react';
+import { isEmpty } from 'lodash';
 
 export const MessageArea = () => {
 	const isMobile = useMobile();
 	const [isDrawerOpened, setDrawerOpen] = useAtom(drawerAtom);
 	const currentThread = useAtomValue(currentThreadAtom);
 	const createThread = useSetAtom(createNewThreadAtom);
+	const llmTokens = useAtomValue(llmTokensAtom);
+	const dontHaveKey = Object.values(llmTokens).every((value) => isEmpty(value.trim()));
 
 	return (
 		<div className={`flex flex-col w-full overflow-auto ${isMobile ? 'h-full' : 'w-full'}`}>
-			<p className='bg-gray-200 text-center text-xs font-mono'>
-				To enable chatting with ChatGPT or Claude, you must enter your API key in the Settings.
-			</p>
+			{dontHaveKey && (
+				<p className='bg-gray-200 text-center text-xs font-mono'>
+					To enable chatting with ChatGPT or Claude, you must enter your API key in the Settings.
+				</p>
+			)}
 			{isMobile && (
 				<div className='sticky top-0 left-0 right-0 px-4 flex flex-row items-center justify-between'>
 					<Burger
