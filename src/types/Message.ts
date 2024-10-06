cd9fb321d2fbbb3cd3f0cc8a0ea4c806be13b9c8
dat.tran <ttdat.nt@gmail.com>
2024-10-06 08:53:56 +0700

    support image

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -1,8 +1,13 @@
+import { FileWithPath } from '@mantine/dropzone';
+
+export type CustomFileWithPath = { file: FileWithPath; id: string };
+
 export type Thread = {
 	id: string;
 	title: string;
 	messages: Record<string, Message>;
 	timestamp: string;
+	files?: CustomFileWithPath[];
 };
 
 export type Message = {
