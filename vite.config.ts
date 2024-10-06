306edb82c4c4aec6e26f85e3c8a5f01c9458241b
dat.tran <ttdat.nt@gmail.com>
2024-10-06 09:11:44 +0700

    upgrade tauri to v2 stable

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,36 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { internalIpV4 } from 'internal-ip';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
+const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-	plugins: [react()],
-	worker: {
-		// format: 'es',
-	},
+  plugins: [react()],
 	resolve: {
 		alias: {
 			'@': '/src',
 		},
 	},
 	base: './',
-
-	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-	//
-	// 1. prevent vite from obscuring rust errors
-	clearScreen: false,
-	// 2. tauri expects a fixed port, fail if that port is not available
-	server: {
-		port: 1420,
-		strictPort: true,
-		host: mobile ? '0.0.0.0' : false,
-		hmr: mobile
-			? {
-					protocol: 'ws',
-					host: await internalIpV4(),
-					port: 1421,
-				}
-			: undefined,
-		watch: {
-			// 3. tell vite to ignore watching `src-tauri`
-			ignored: ['**/src-tauri/**'],
-		},
-	},
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
 }));
